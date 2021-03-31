3ae2b40d1873c3e6ab7391961a1f6de5b0e7b64d
kad-mesdat <mesdat@kadaster.nl>
2021-03-31 19:50:55 +0200

    update van het model tbv interne review

---
+++ informatiemodel/respec/README.md
@@ -0,0 +1,2 @@
+# Catalogus Zorgeloos Vastgoed
+De plek om de catalogus voor het Conceptueel informatiemodel van Zorgeloos Vastgoed te maken. Deze wordt uiteindelijk gepubliceerd op http://developer.zorgeloosvastgoed.nl/informatiemodel/ en wordt via https://dev.kadaster.nl/html/im-informatiemodellen/zvg/index.html lokaal als draft gepubliceerd.
---
+++ informatiemodel/respec/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+//  specType: "IM",
+  publishDate: "2021-01-21",
+  editors: [{
+    name: "Modellenbureau",
+    company: "Kadaster",
+    companyURL: "www.kadadaster.nl",
+  }],
+  shortName: "Kadaster",
+  pubDomain: "Bestuurlijke Gebieden",
+  subtitle: "Review draft",
+	github: "https://github.com/kadaster/imkad",
+logos: [{
+		src: 'Kadaster_logo.png',
+		href: "https://developers.kadaster.nl/schemas",
+		alt: "Kadaster logo",
+		id: 'KadasterLogo',
+  }],
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
